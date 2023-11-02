e97b0d4acb84126d9c23330bb13d1cdfc280b987
Clark Rodriguez Millan <740394@student.dusd.net>
2023-11-02 15:26:41 -0700

    barley started tile must do
    must make rest of tile maps and sprite stuff

--- RubyPer7Clark/Assets/scriplet/Rudy Controls.cs
+++ RubyPer7Clark/Assets/scriplet/Rudy Controls.cs
@@ -16,8 +16,8 @@ public class RudyControls : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.3f * horizontal * Time.deltaTime;
-        position.y = position.y + 0.3f * vertical * Time.deltaTime;
+        position.x = position.x + 5.3f * horizontal * Time.deltaTime;
+        position.y = position.y + 5.3f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
