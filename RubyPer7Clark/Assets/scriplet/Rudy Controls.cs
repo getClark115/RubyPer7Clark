467b7077219723baee154d859b13f2111df837ef
Clark Rodriguez Millan <740394@student.dusd.net>
2023-11-15 15:25:25 -0800

    made colider
    did stuff for collider need to finish health collectables

--- RubyPer7Clark/Assets/scriplet/Rudy Controls.cs
+++ RubyPer7Clark/Assets/scriplet/Rudy Controls.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class RudyControls : MonoBehaviour
 {
+    public float speed = 5.3f;
+    public int maxHealth = 55;
+        int currentHealth;
+
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+       rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector2 position = transform.position;
-        position.x = position.x + 5.3f * horizontal * Time.deltaTime;
-        position.y = position.y + 5.3f * vertical * Time.deltaTime;
-        transform.position = position;
+         horizontal = Input.GetAxis("Horizontal");
+         vertical = Input.GetAxis("Vertical");
+        
+        
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0,maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
